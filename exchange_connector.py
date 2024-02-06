de81a5a844c4473aa053dd4c871ea65951ede86d
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2024-02-06 15:14:18 +0100

    [fetching] add logs

--- exchange_connector.py
+++ exchange_connector.py
@@ -6,7 +6,11 @@ import logging
 
 def get_exchange():
     exchange_name = os.getenv("EXCHANGE", "binance")
-    exchange = getattr(ccxt.async_support, exchange_name)()
+    exchange = getattr(ccxt.async_support, exchange_name)(
+        config={
+            "timeout": 20000
+        }
+    )
     exchange.logger.setLevel(logging.INFO)
     return exchange
 
@@ -19,6 +23,12 @@ async def get_tickers(exchange: ccxt.async_support.Exchange) -> list:
             for ticker in response
         ):
             await reload_markets(exchange)
+        usd_pairs = set(
+            ticker["symbol"]
+            for ticker in response
+            if ticker["symbol"].endswith("USDT")
+        )
+        logging.getLogger(f"{exchange.id}").info(f"fetched ticker for {len(usd_pairs)} usd pairs {usd_pairs}")
         return [
             {
                 "symbol": exchange.safe_symbol(ticker["symbol"], marketType="spot"),
@@ -38,5 +48,11 @@ async def get_ohlcv(exchange: ccxt.async_support.Exchange, symbol: str, timefram
 
 
 async def reload_markets(exchange: ccxt.async_support.Exchange):
-    logging.getLogger(f"{exchange.id}").info("Reloading market")
-    return await exchange.load_markets(reload=True)
+    logger = logging.getLogger(f"{exchange.id}")
+    logger.info("Reloading market")
+    await exchange.load_markets(reload=True)
+    currencies = set(
+        market["base"]
+        for market in exchange.markets.values()
+    )
+    logger.info(f"market currencies: {len(currencies)} currencies: {currencies}")
